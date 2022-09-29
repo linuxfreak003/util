5151ee1e31a7d45b40ff5b884273792012fddd64
Jared Chapman <jared.chapman@tcnbroadcasting.com>
2022-09-29 13:52:37 -0600

    added more tests

--- slice/slice.go
+++ slice/slice.go
@@ -14,8 +14,17 @@ func Map[A, B any](as []A, f func(A) B) []B {
 	return bs
 }
 
+// Fold folds the slice into a single value
+// using the given function and accumulator
+func Fold[A, B any](as []A, acc B, f func(A, B) B) B {
+	for _, a := range as {
+		acc = f(a, acc)
+	}
+	return acc
+}
+
 // Contains checks for the existence of an
-// element in a slice
+// element in the slice
 func Contains[T comparable](l []T, v T) bool {
 	for _, t := range l {
 		if t == v {
@@ -36,7 +45,7 @@ func Shuffle[T any](l []T) []T {
 	return l
 }
 
-// Filter filters a slice using a given function
+// Filter filters a slice using the given function
 func Filter[T any](in []T, f func(T) bool) (out []T) {
 	for _, t := range in {
 		if f(t) {
@@ -44,4 +53,41 @@ func Filter[T any](in []T, f func(T) bool) (out []T) {
 		}
 	}
 	return out
+}
+
+// Deduplicate deduplicates a slice
+func Deduplicate[T comparable](in []T) []T {
+	seen := map[T]struct{}{}
+	result := []T{}
+	for _, el := range in {
+		if _, ok := seen[el]; ok {
+			continue
+		}
+		result = append(result, el)
+		seen[el] = struct{}{}
+	}
+	return result
+}
+
+// Remove will remove from the slice
+// the first match of the given value
+func Remove[T comparable](xs []T, d T) []T {
+	for i, x := range xs {
+		if x == d {
+			return append(xs[:i], xs[i+1:]...)
+		}
+	}
+	return xs
+}
+
+// RemoveAll removes all occurances of
+// the given value in the slice
+func RemoveAll[T comparable](xs []T, v T) []T {
+	var result []T
+	for _, x := range xs {
+		if x != v {
+			result = append(result, x)
+		}
+	}
+	return result
 }
--- util_test.go
+++ util_test.go
@@ -11,13 +11,44 @@ import (
 func TestRound(t *testing.T) {
 	rounded := util.Round(1.23454, 3)
 	assert.Equal(t, rounded, 1.235)
+
+	rounded = util.Round(1.23454, 4)
+	assert.Equal(t, rounded, 1.2345)
+
+	rounded = util.Round(1.23454, 5)
+	assert.Equal(t, rounded, 1.23454)
 }
 
+//revive:disable:empty-lines
 func TestSlices(t *testing.T) {
 	assert := assert.New(t)
 	t.Run("Map function", func(t *testing.T) {
 		in := []int{1, 2, 3, 4}
 		out := slice.Map(in, func(i int) int { return i * i })
 		assert.Equal(out, []int{1, 4, 9, 16})
+	})
+
+	t.Run("Fold function", func(t *testing.T) {
+		in := []int{1, 2, 3, 4}
+		out := slice.Fold(in, 0, func(x int, y int) int { return x + y })
+		assert.Equal(out, 10)
+	})
+
+	t.Run("Deduplicate function", func(t *testing.T) {
+		in := []int{1, 2, 3, 4, 1, 2, 3, 4}
+		out := slice.Deduplicate(in)
+		assert.Equal(out, []int{1, 2, 3, 4})
+	})
+
+	t.Run("Remove function", func(t *testing.T) {
+		in := []int{1, 1, 1, 2, 3, 4, 1, 1, 1}
+		out := slice.Remove(in, 1)
+		assert.Equal(out, []int{1, 1, 2, 3, 4, 1, 1, 1})
+	})
+
+	t.Run("RemoveAll function", func(t *testing.T) {
+		in := []int{1, 1, 1, 2, 3, 4, 1, 1, 1}
+		out := slice.RemoveAll(in, 1)
+		assert.Equal(out, []int{2, 3, 4})
 	})
 }
